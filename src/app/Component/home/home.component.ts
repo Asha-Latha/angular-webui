830546fed124586d2514f1956109388d33db8978
AshaLatha <ashapedapalli18@gmail.com>
2019-01-29 14:04:32 +0530

    new repo

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -1,17 +1,76 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user/user.service';
+import 'rxjs/add/operator/catch';
+ import 'rxjs/add/operator/map';
+ import 'rxjs/add/observable/throw';
+ import 'rxjs/add/operator/timeoutWith';
+ import 'rxjs/add/operator/timeout';
+ import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [
+    UserService
+]
 })
 export class HomeComponent implements OnInit {
+  
+  errorMessage: string;
+  warning:string;
+  email : string;
 
-  constructor() { }
+  constructor(private userService:UserService) { }
 
   ngOnInit() {
   }
-
+  UserExists()
+  {
+    this.userService.userExist(this.email).subscribe(users => 
+    {
+      if (users.isSuccess===true) 
+      {
+          console.log("users",users);
+          localStorage.setItem('userData', JSON.stringify(users.data));
+        	//this.router.navigate(['/login/' + this.email]);			
+      } 
+      else 
+      {
+        alert(users.message);
+         this.warning = String(users);
+         //this.router.navigate(['/signup/']);
+      }
+    },	
+    error => 
+    {
+      this.errorMessage = <any>error;
+      // console.log(this.errorMessage);
+      if (this.errorMessage === 'Invalid Username') 
+      {
+				console.log(this.errorMessage);           
+      }
+      else 
+      {
+	  		console.log(this.errorMessage);
+      }
+    });
+  }
 }
 
 
@@ -34,8 +93,3 @@ export class HomeComponent implements OnInit {
 //   });
 // });
 
-//   $("#verify").click(function()
-//   {
-//       window.location.href='../Verify/verify.html?';
-//   });
-// });
